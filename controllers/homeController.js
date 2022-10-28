1cef3f45c0bbac0b51c1cb7f0b0352b64f808a8a
Tasnim Ahmed <tasnimahmed@iut-dhaka.edu>
2022-10-28 10:20:46 +0600

    Added authentication with passportJS

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,26 @@
 const getHome = (req, res) => {
-  res.render("home");
+  res.render('index', { title: 'Home' });
 };
 
-module.exports = { getHome };
+const getLogin = (req, res) => {
+  res.render('login', { title: 'Login' });
+};
+
+const postLogin = (req, res) => {
+  console.log(req.user);
+}
+
+const getDashboard = (req, res) =>{
+  res.render('dashboard', { title: 'Dashboard' })
+}
+
+const logOut = (req, res)=>{
+  req.logout(()=>{
+  console.log("Logging out!")
+  });
+  res.redirect('/');
+}
+
+
+
+module.exports = { getHome, getLogin, postLogin, getDashboard, logOut };
--- routes.js
+++ routes.js
@@ -2,10 +2,25 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("./controllers/homeController");
 const bookController = require("./controllers/bookController");
+const connectEnsureLogin = require('connect-ensure-login');
+const passport = require('passport');
+
+
+
+
 
 router.get("/", homeController.getHome);
+router.get('/login', homeController.getLogin);
+router.get('/logout', homeController.logOut);
 router.get("/book-list", bookController.getBookList);
 router.get("/books", bookController.getBook);
+router.get('/dashboard', connectEnsureLogin.ensureLoggedIn(), homeController.getDashboard);
 router.post("/books", bookController.postBook);
+router.post(
+    '/login',
+    passport.authenticate('local', {
+      failureRedirect: '/login',
+      successRedirect: '/dashboard',
+    }),homeController.postLogin);
 
 module.exports = router;
